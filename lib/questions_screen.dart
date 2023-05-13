b8492b74e8a2439572ef3343e23d1f2b632b1476
bentalla108 <tallambaye108@gmail.com>
2023-05-13 14:26:58 +0000

    Change $creen Code

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_app/answer_button.dart';
+import 'package:quizz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,35 +12,40 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenSate extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
-        width: double.infinity,
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'The Questions...',
-              style: TextStyle(
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
                 color: Colors.white,
               ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 20,
             ),
-            AnswerButton(
-              answerText: 'Answer 1',
-              onTap: () {},
-            ),
-            AnswerButton(
-              answerText: 'Answer 1',
-              onTap: () {},
-            ),
-            AnswerButton(
-              answerText: 'Answer 1',
-              onTap: () {},
-            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
+            }),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
